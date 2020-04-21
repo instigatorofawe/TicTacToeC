42c5f58ae36421e3878bb28d152ca19da5c9cac1
Ran Liu <rliu14@jhu.edu>
2020-04-21 16:19:03 -0400

    Add Readme, Board library

---
+++ util/board.c
@@ -0,0 +1,5 @@
+//
+// Created by Ran Liu on 4/21/2020.
+//
+
+#include "board.h"
---
+++ util/board.h
@@ -0,0 +1,8 @@
+//
+// Created by Ran Liu on 4/21/2020.
+//
+
+#ifndef TICTACTOEC_BOARD_H
+#define TICTACTOEC_BOARD_H
+
+#endif //TICTACTOEC_BOARD_H
