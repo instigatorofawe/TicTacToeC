35cba3031fef9c0976ea07a3c67daac85da5ed18
Ran Liu <rliu14@jhu.edu>
2020-10-17 00:40:44 -0400

    Unit tests for hashmap

--- test/test_hashmap.cpp
+++ test/test_hashmap.cpp
@@ -17,3 +17,18 @@ TEST(test_hashmap,test_init) {
     HashMap* map = init_hashmap(16);
     delete_hashmap(map);
 }
+
+TEST(test_hashmap,test_function) {
+    unsigned int hashes[] = {0, 1, 2, 3, 4, 5, 6, 18};
+    int values[] = {0, 1, 2, 3, 4, 5, 6, 18};
+    HashMap* map = init_hashmap(16);
+
+    for (int c = 0; c < 6; c++) {
+        ASSERT_FALSE(hashmap_contains(map, hashes[c]));
+        hashmap_insert(map, hashes[c], &values[c]);
+        ASSERT_TRUE(hashmap_contains(map, hashes[c]));
+        ASSERT_EQ(*(int*)hashmap_get(map, hashes[c]), values[c]);
+    }
+
+    delete_hashmap(map);
+}
